4ca85a17c7d5b5ed2197c5df4d1039d250f7fded
Jyoti Bajaj <aashi.kwatra@gmail.com>
2015-06-24 19:15:20 -0700

    spinner sample with on click event listener

--- SpinnerSample/app/src/main/java/letsdecode/com/spinnersample/MyActivity.java
+++ SpinnerSample/app/src/main/java/letsdecode/com/spinnersample/MyActivity.java
@@ -2,13 +2,28 @@ package letsdecode.com.spinnersample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class MyActivity extends Activity {
+public class MyActivity extends Activity implements AdapterView.OnItemSelectedListener {
 
     Spinner spinner;
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        TextView v = (TextView)view;
+        Toast.makeText(this,v.getText(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +31,10 @@ public class MyActivity extends Activity {
         setContentView(R.layout.activity_my);
         // get a reference of the spinner
         spinner = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.descriptions, android.R.layout.simple_spinner_item);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.descriptions,
+                android.R.layout.simple_spinner_item);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
     }
 
     /*1.creat the data source.
