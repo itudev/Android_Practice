e07da6f039d98a8376e1b70bbd1aa6d5a285d118
Jyoti Bajaj <aashi.kwatra@gmail.com>
2015-07-02 14:36:33 -0700

    added EditText validation with TextWatcher

--- BoyFriendTracker/app/src/main/java/letsdecode/com/husbandtracker/MyActivity.java
+++ BoyFriendTracker/app/src/main/java/letsdecode/com/husbandtracker/MyActivity.java
@@ -4,7 +4,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MyActivity extends Activity {
@@ -14,33 +20,92 @@ public class MyActivity extends Activity {
     public static String secretCodeValue;
 
     EditText secretCodeEdit;
+    Button saveButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //xml object
         setContentView(R.layout.activity_my);
-        //capturing EditText
-        secretCodeEdit = (EditText) findViewById(R.id.secretCodeEdit);
-        //retreiving text form edit text
-        String secretCode = secretCodeEdit.getText().toString();
-        /*
+/*
         In order to use shared preferences , you have to call a method getSharedPreferences()
         that returns a SharedPreference instance pointing to the file that contains the values of preferences.
          */
         //first parameter is key
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
-        /*in order to save something in Editor object get an instance of Editor by calling edit().
+        //capturing EditText
+        secretCodeEdit = (EditText) findViewById(R.id.secretCodeEdit);
+        saveButton = (Button) findViewById(R.id.save);
+        saveButton.setEnabled(false);
 
-         */
-        SharedPreferences.Editor editor = sharedpreferences.edit();
-        editor.putString(SECRET_CODE, secretCode);
-        editor.apply();
-        //retrieving value from Editor instance corresponding to particular key.
-        secretCodeValue = sharedpreferences.getString(SECRET_CODE, "");
+           /* Set Text Watcher listener */
+
+        secretCodeEdit.addTextChangedListener(secretCodeWatcher);
+        //retrieving text form edit text
 
 
     }
 
+    /*implementing TextWatcher and OnClickListener
+
+     */
+    private final TextWatcher secretCodeWatcher = new TextWatcher() {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            secretCodeEdit.setVisibility(View.VISIBLE);
+        }
+
+        public void afterTextChanged(Editable s) {
+            final String secretCode = secretCodeEdit.getText().toString();
+            if (secretCode.length() == 4) {
+                saveButton.setEnabled(true);
+                saveButton.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+
+                        SharedPreferences.Editor editor = sharedpreferences.edit();
+
+                        //String secretCode = secretCodeEdit.getText().toString();
+                      /*in order to save something in Editor object get an instance of Editor by calling edit()*/
+                        editor.putString(SECRET_CODE, secretCode);
+                        editor.apply();
+                        secretCodeValue = sharedpreferences.getString(SECRET_CODE, "");
+                        Log.d("code", secretCodeValue);
+
+                        //retrieving value from Editor instance corresponding to particular key.
+
+                    }
+
+
+                });
+//            secretCodeValue = sharedpreferences.getString(SECRET_CODE, "");
+//            Log.d("code", secretCodeValue);
+
+
+            } else {
+                Toast.makeText(getApplicationContext(), "enter four digits", Toast.LENGTH_SHORT).show();
+                saveButton.setEnabled(false);
+            }
+
+
+        }
+    };
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
